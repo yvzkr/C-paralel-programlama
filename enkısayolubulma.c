2cbe04b75a9d722139142b875658c97ac9bec1fa
yvzkr <yavuzkuru3@gmail.com>
2017-02-27 20:33:46 +0300

    proje baslangici

---
+++ enkısayolubulma.c
@@ -0,0 +1,41 @@
+/*
+ * mapgame.c
+ * 
+ * Copyright 2017 yavuz <yavuz@yavuz-virtual-machine>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+int map[6][6]={1,1,0,0,0,0, 1,1,1,0,0,0, 0,1,1,1,1,0, 0,0,1,1,1,1, 0,0,0,0,0,1, 0,0,0,0,0,1};
+int find(int x,int y){
+	
+	
+	return 5;
+}
+
+int main(int argc, char **argv)
+{
+	
+	find(52,4);
+	
+	return 0;
+}
+
+
